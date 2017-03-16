72e6f7b4942e9cfeb98900772c17ec94e2608bc1
Jan <bachmann.heinsberg@gmail.com>
2017-03-16 23:39:48 +0100

    DB connection now based on configuration

--- models/Base.js
+++ models/Base.js
@@ -1,5 +1,14 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test');
+var config = require('../config/config');
+
+var mongo = config().mongo;
+var uri = 'mongodb://' + mongo.host + ':' + mongo.port + '/' + mongo.db;
+var options = {
+  user: mongo.username,
+  pass: mongo.password
+};
+
+mongoose.connect(uri , options);
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.once('open', function() {
   console.log('Connected to Database');
